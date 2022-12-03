a47d633cafac204b7c10c42f764a9ff90df8e8ea
ThisIsMyY <boksenyanu00@gmail.com>
2022-12-03 23:13:55 +0300

    частичная третья лаба по TP

---
+++ TP_lab3/TP_lab3/MyString.cs
@@ -0,0 +1,282 @@
+﻿using System;
+
+namespace TP_lab3
+{
+    internal class MyString
+    {
+        private string result = "";
+        private int length = 0;
+        public string Result { get { return result; } set { result = value; } }
+        public int Length { get { return length; } set { length = value; } }
+
+        //funtion-indexator
+        public char this[int index] { get { return this[index]; } }
+
+
+        //Constructors
+        public MyString(char ch, int repeatNumber)
+        {
+            for(int i = 0; i < repeatNumber; i++)
+            {
+                result += ch;
+                length++;
+            }
+        }
+        public MyString(char[] chArr)
+        {
+            foreach (char ch in chArr)
+            {
+                result += ch;
+                length++;
+            }
+        }
+        public MyString(char[] chArr, int startIndex, int endIndex)
+        {
+            for(int i = startIndex; i < endIndex; i++)
+            {
+                result += chArr[i];
+                length++;
+            }
+        }
+        public MyString(string str1, string str2)
+        {
+            result = str1 + str2;
+            foreach (char c in result)
+            {
+                length++;
+            }
+        }
+
+        public MyString(string str)
+        {
+            result = str;
+            foreach (char c in result)
+            {
+                length++;
+            }
+        }
+        //methods
+
+        //string joining
+        public static string Join(string separator, string[] array)
+        {
+            string separatedArray = "";
+            if(array.Length > 1)
+            {
+                for (int s = 0; s < array.Length - 1; s++)
+                {
+                    separatedArray += array[s] + separator;
+                }
+            }
+            separatedArray += array[array.Length - 1];
+            return separatedArray;
+        }
+        public static string Join(char separator, string[] array)
+        {
+            string separatedArray = "";
+            if (array.Length > 1)
+            {
+                for (int s = 0; s < array.Length - 1; s++)
+                {
+                    separatedArray += array[s] + separator;
+                }
+            }
+            separatedArray += array[array.Length - 1];
+            return separatedArray;
+        }
+
+        //Spaces counter
+        public int SpacesCounter()
+        {
+            int spacesCount = 0;
+            foreach (char ch in result)
+            {
+                if(ch == ' ')
+                {
+                    spacesCount++;
+                }
+            }
+            return spacesCount;
+        }
+
+        //check for equality
+        public bool Equals(string value)
+        {
+            if (result == value)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static bool Equals(string value1, string value2)
+        {
+            if (value1 == value2)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        //Concat
+        public static string Concat(params string[] values)
+        {
+            string concatString = "";
+            foreach(string str in values)
+            {
+                concatString += str;
+            }
+            return concatString;
+        }
+
+        //Contains
+        public bool Contains(string value)
+        {
+            string substring = "";
+            for (int i = 0; i < length; i++)
+            {
+                if (i <= value.Length - 1)
+                {
+                    if (result[i] == value[i])
+                    {
+                        substring += value[i];
+                        if (substring == value)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else return false;
+            }
+            return false;
+        }
+
+        //StartsWith
+        public bool StartsWith(string value)
+        {
+            if(length < value.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (result[i] != value[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //EndsWith
+        public bool EndsWith(string value)
+        {
+            if (length < value.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = value.Length-1, j = 1; i > 0; i--, j++)
+                {
+                    if (result[length-j] != value[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+        //IndexOf
+        public int IndexOf(char value)
+        {
+            for(int ch = 0; ch < length; ch++)
+            {
+                if(result[ch] == value)
+                {
+                    return ch;
+                }
+            }
+            return -1;
+        }
+        public int IndexOf(string value)
+        {
+            int index = -1;
+            int count = 0;
+            for (int ch = 0; ch < length; ch++)
+            {
+                for(int j = 0; j < value.Length; j++)
+                {
+                    //Console.WriteLine("ch: " + ch + ", j: " + j + ", index: " + index + ", count: " + count + ", first: " + result[ch] + ", second: " + value[j]);
+                    if (result[ch+j] == value[j])
+                    {
+                        count++;
+                        if (count == 1)
+                        {
+                            index = ch;
+                        }
+
+                        if (count == value.Length)
+                        {
+                            return index;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                        break;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        //LasrIndexOf
+        public int LastIndexOf(char value)
+        {
+            for (int ch = length; ch > 0; ch--)
+            {
+                if (result[ch] == value)
+                {
+                    return ch;
+                }
+            }
+            return -1;
+        }
+        public int LastIndexOf(string value)
+        {
+            int index = -1;
+            int count = 0;
+            for (int ch = length - 1; ch > 0; ch--)
+            {
+                for (int j = value.Length - 1; j > 0; j--)
+                {
+                    //Console.WriteLine("ch: " + ch + ", j: " + j + ", index: " + index + ", count: " + count + ", first: " + result[ch] + ", second: " + value[j]);
+                    if (result[ch - value.Length - j] == value[j])
+                    {
+                        count++;
+                        if (count == 1)
+                        {
+                            index = ch;
+                        }
+
+                        if (count == value.Length)
+                        {
+                            return index;
+                        }
+                    }
+                    else
+                    {
+                        count = 0;
+                        break;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ TP_lab3/TP_lab3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TP_lab3
+{
+    using mystring = MyString;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string creating
+            mystring string1 = new MyString('a', 6);
+            mystring string2 = new MyString(new char[]{'a','b'});
+            mystring string3 = new MyString(new char[]{'a','b','c','d','e','f','g','h','i','j'}, 1, 8);
+            mystring string4 = new MyString("hello world. See ya!");
+
+            //Join method
+            string[] stringArray = { "string1", "string2", "string3", "string4", "string5", "string6"};
+            mystring string5 = new MyString(MyString.Join(", ", stringArray));
+            mystring string6 = new MyString(MyString.Join(',', stringArray));
+
+            //Concat method
+            mystring string7 = new MyString(MyString.Concat("str1", "str2", "str3", "str4"));
+
+            //strings output
+            Console.WriteLine("\nstring1: " + string1.Result);
+            Console.WriteLine("\nstring2: " + string2.Result);
+            Console.WriteLine("\nstring3: " + string3.Result);
+            Console.WriteLine("\nstring4: " + string4.Result);
+            Console.WriteLine("\nstring5: " + string5.Result);
+            Console.WriteLine("\nstring6: " + string6.Result);
+            Console.WriteLine("\nstring7: " + string7.Result);
+
+            //get length
+            Console.WriteLine("\nstring length of string1: " + string1.Length);
+
+            //Spaces counter method
+            Console.WriteLine("\ncount of spaces in string5: " + string4.SpacesCounter());
+
+            //Equals method
+            Console.WriteLine("\nis string3 equal to \"abcdefghij\"? " + string3.Equals("abcdefghij"));
+            Console.WriteLine("\nis string3 equal to \"bcdefgh\"? " + string3.Equals("bcdefgh"));
+            Console.WriteLine("\nare two string equals? " + MyString.Equals("string1", "string2"));
+
+            //Contains method
+            Console.WriteLine("\nis current string contains substring? " + string4.Contains("hello world"));
+
+            //StartsWith method
+            Console.WriteLine("\nis string \" " + string4.Result + "\" starts with substring \"hello\"? " + string4.StartsWith("hello"));
+
+            //EndsWith method
+            Console.WriteLine("\nis string \" "+string4.Result+ "\" ends with substring \"See ya!\"? " + string4.EndsWith("See ya!"));
+
+            //IndexOf
+            if(string4.IndexOf("See") != -1)
+            {
+                Console.WriteLine("\nfirst occurance of substring in current string has index: " + string4.IndexOf("See"));
+            }
+            else
+            {
+                Console.WriteLine("\ncurrent string has not such a substring");
+            }
+
+            if (string4.LastIndexOf("See") != -1)
+            {
+                Console.WriteLine("\nfirst occurance of substring in current string has index: " + string4.LastIndexOf("See"));
+            }
+            else
+            {
+                Console.WriteLine("\ncurrent string has not such a substring");
+            }
+
+            //funtion-indexator
+            Console.WriteLine("\nstring2[1]: " + string2.Result[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
