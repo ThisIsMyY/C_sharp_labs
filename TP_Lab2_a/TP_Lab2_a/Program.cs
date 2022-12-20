3062c5e50933c99b90314b8b99e096b279206315
ThisIsMyY <boksenyanu00@gmail.com>
2022-12-21 00:33:10 +0300

    доделал лабу 2а

--- TP_Lab2_a/TP_Lab2_a/Program.cs
+++ TP_Lab2_a/TP_Lab2_a/Program.cs
@@ -8,12 +8,13 @@ namespace TP_Lab2_a
 {
     internal class Program
     {
-        public static void SystemCreating()
+        public static void SystemCreatingAndCalculation()
         {
-            char[] operators = {'+', '-'};
-
-            Console.Write("Enter N: ");
+            Console.Write("Enter N (2 - 3): ");
             int n = int.Parse(Console.ReadLine());
+            if(n < 2) n = 2;
+            else if(n > 3) n = 3;
+
             string[] system = new string[n];
             double[,] matrix = new double[n,n+1];
 
@@ -33,7 +34,7 @@ namespace TP_Lab2_a
                     for (int j = 1; j <= n; j++)
                     {
                         string elem;
-                        double nextNum = rand.Next(1, 100) * ((double)rand.Next(1, 10) / 10);
+                        double nextNum = rand.Next(-100, 100) * ((double)rand.Next(1, 10) / 10);
 
                         if (nextNum != 1)
                         {
@@ -44,9 +45,17 @@ namespace TP_Lab2_a
                             elem = "x" + j;
                         }
 
-                        if (j != n)
+                        //operator adding
+                        if (j > 1)
                         {
-                            elem += " " + operators[rand.Next(0, 2)] + " ";
+                            if(nextNum < 0)
+                            {
+                                elem = " - " + (nextNum * -1).ToString();
+                            }
+                            else
+                            {
+                                elem = " + " + elem;
+                            }
                         }
 
                         system[i] += elem;
@@ -54,21 +63,13 @@ namespace TP_Lab2_a
                         matrix[i, j - 1] = nextNum;
 
                     }
-                    double ending = rand.Next(1, 100) * ((double)rand.Next(1, 10) / 10);
+
+                    double ending = rand.Next(-100, 100) * ((double)rand.Next(1, 10) / 10);
                     system[i] += " = " + ending;
                     matrix[i, n] = ending;
-
-                    Console.WriteLine(system[i]);
                 }
 
-                for (int i = 0; i < n; i++)
-                {
-                    Console.WriteLine();
-                    for (int j = 0; j <= n; j++)
-                    {
-                        Console.Write(matrix[i, j] + " ");
-                    }
-                }
+                SystemDisplay(system, matrix, n);
             }
 
             //Manual input
@@ -110,55 +111,153 @@ namespace TP_Lab2_a
                             elem = "x" + j;
                         }
 
-                        if (j != n)
+                        //operator adding
+                        if (j > 1)
                         {
-                            elem += " " + operators[rand.Next(0, 2)] + " ";
+                            if (j > 1)
+                            {
+                                if (matrix[i, j - 1] < 0)
+                                {
+                                    elem = " - " + (matrix[i, j - 1] * -1).ToString();
+                                }
+                                else
+                                {
+                                    elem = " + " + elem;
+                                }
+                            }
                         }
 
                         system[i] += elem;
                     }
-                    Console.WriteLine(system[i]);
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    Console.WriteLine();
-                    for (int j = 0; j < n; j++)
+
+                    Console.Write("last elem  = ");
+                    string lastSystemElement = Console.ReadLine();
+                    bool isDecimalNumber = false;
+
+                    for (int ch = 0; ch < lastSystemElement.Length; ch++)
                     {
-                        Console.Write(matrix[i, j] + " ");
+                        if (lastSystemElement[ch] == '/')
+                        {
+                            isDecimalNumber = true;
+                            string[] fractionElemPart = lastSystemElement.Split('/');
+                            system[i] += " = " + Math.Round(double.Parse(fractionElemPart[0]) / double.Parse(fractionElemPart[1]), 2);
+                            matrix[i, n] = Math.Round(double.Parse(fractionElemPart[0]) / double.Parse(fractionElemPart[1]), 2);
+                            break;
+                        }
+                    }
+                    if (isDecimalNumber == false)
+                    {
+                        system[i] += " = " + lastSystemElement;
+                        matrix[i, n] = double.Parse(lastSystemElement);
                     }
                 }
+                SystemDisplay(system, matrix, n);
             }
-            double[] variables = Determinal(matrix, n);
+
+            //system variables calculation
+            Determinal(matrix, n);
         }
 
-        public static double[] Determinal(double[,] matrix, int n)
+        public static void SystemDisplay(string[] system, double[,] matrix, int n)
         {
-            /*
-            
-            a1, b1, c1
-            a2, b2, c2
-            a3, b3, c3
-
-            */
-            double[] variables = new double[n];
-            double mainDet = 0;
+            //system output
+            Console.WriteLine("\nSystem:-----------------------\n");
             for (int i = 0; i < n; i++)
             {
-                double subMatrixMultiply = 1;
-
-                for (int el = 0; el < n; el++)
-                {
-                    subMatrixMultiply *= matrix[el + 1, el + 1];
-                }
-
-                mainDet += matrix[0, i] * subMatrixMultiply;
+                Console.WriteLine(system[i]);
             }
-            return variables;
+
+            //matrix output
+            Console.WriteLine("\nMatrix:-----------------------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j <= n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+            }
+        }
+
+        public static void Determinal(double[,] matrix, int n)
+        {
+            double[] variables = new double[n];
+            NumarRational mainDet;
+            if (n == 2)
+            {
+                NumarRational a1 = new NumarRational(matrix[0, 0]);
+                NumarRational a2 = new NumarRational(matrix[1, 0]);
+
+                NumarRational b1 = new NumarRational(matrix[0, 1]);
+                NumarRational b2 = new NumarRational(matrix[1, 1]);
+
+                NumarRational n1 = new NumarRational(matrix[0, n]);
+                NumarRational n2 = new NumarRational(matrix[1, n]);
+
+                mainDet = a1 * b2 - b1 * a2;
+                NumarRational detX1 = (n1 * b2 - b1 * n2) / mainDet;
+                NumarRational detX2 = (a1 * n2 - n1 * a2) / mainDet;
+
+                Console.WriteLine("\n-----------------------");
+                Console.WriteLine("x1 = " + Math.Round(detX1.NumValue, 3));
+                Console.WriteLine("x2 = " + Math.Round(detX2.NumValue, 3));
+            }
+            /*
+            
+            a1, n, c1
+            a2, n, c2
+            a3, n, c3
+
+            */
+            else if (n == 3)
+            {
+                NumarRational a1 = new NumarRational(matrix[0, 0]);
+                NumarRational a2 = new NumarRational(matrix[1, 0]);
+                NumarRational a3 = new NumarRational(matrix[2, 0]);
+
+                NumarRational b1 = new NumarRational(matrix[0, 1]);
+                NumarRational b2 = new NumarRational(matrix[1, 1]);
+                NumarRational b3 = new NumarRational(matrix[2, 1]);
+
+                NumarRational c1 = new NumarRational(matrix[0, 2]);
+                NumarRational c2 = new NumarRational(matrix[1, 2]);
+                NumarRational c3 = new NumarRational(matrix[2, 2]);
+
+                NumarRational n1 = new NumarRational(matrix[0, n]);
+                NumarRational n2 = new NumarRational(matrix[1, n]);
+                NumarRational n3 = new NumarRational(matrix[2, n]);
+
+                NumarRational detX1;
+                NumarRational detX2;
+                NumarRational detX3;
+
+                mainDet = a1 * (b2 * c3 - c2 * b3);
+                mainDet = mainDet - b1 * (a2 * c3 - c2 * a3);
+                mainDet = mainDet + c1 * (a2 * b3 - b2 * a3);
+
+                detX1 = n1 * (b2 * c3 - c2 * b3);
+                detX1 = detX1 - b1 * (n2 * c3 - c2 * n3);
+                detX1 = detX1 + c1 * (n2 * b3 - b2 * n3);
+
+                detX2 = b1 * (n2 * c3 - c2 * n3);
+                detX2 = detX2 - n1 * (a2 * c3 - c2 * a3);
+                detX2 = detX2 + c1 * (a2 * n3 - n2 * a3);
+
+                detX3 = a1 * (b2 * n3 - n2 * b3);
+                detX3 = detX3 - b1 * (a2 * n3 - n2 * a3);
+                detX3 = detX3 + n1 * (a2 * b3 - b2 * a3);
+
+
+                Console.WriteLine("\n-----------------------");
+                Console.WriteLine("x1 = " + detX1.NumValue);
+                Console.WriteLine("x2 = " + detX2.NumValue);
+                Console.WriteLine("x3 = " + detX3.NumValue);
+            }
         }
 
         static void Main(string[] args)
         {
-            SystemCreating();
+            SystemCreatingAndCalculation();
 
             Console.ReadLine();
         }
---
+++ TP_Lab4/TP_Lab4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Lab4
+{
+    internal class Program
+    {
+        public static void FieldGenerating()
+        {
+            Console.WriteLine("-----------");
+            Console.SetCursorPosition(0, 17);
+            Console.WriteLine("-----------");
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < 18; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("|");
+                Console.SetCursorPosition(11, i);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(0, 1);
+        }
+        public static void FieldUpdate()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if(i> 0) Console.WriteLine();
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.SetCursorPosition(j+1, i+1);
+                    Console.Write("0");
+                }
+            }
+        }
+        public static void Play()
+        {
+            for(int i = 1; i <= 16; i++)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.F10)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(1, 1);
+                FieldUpdate();
+                Console.SetCursorPosition(4, i);
+                Console.Write("♦♦♦♦");
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            FieldGenerating();
+            Play();
+
+            Console.ReadLine();
+        }
+    }
+}
